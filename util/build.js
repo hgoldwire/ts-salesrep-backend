659c53c927d6f00bbf6a0ec0762eb02a70f3eaa1
Henry Goldwiwe <henry.goldwire@roivant.com>
2019-08-02 16:31:38 -0400

    gitignore built artifacts in dist/ stricter typescript

--- util/build.js
+++ util/build.js
@@ -4,8 +4,8 @@ const childProcess = require('child_process');
 
 // remove current build, and create new one
 fs.removeSync('./dist/');
-fs.copySync('./src/public', './dist/public');
-fs.copySync('./src/views', './dist/views');
+// fs.copySync('./src/public', './dist/public');
+// fs.copySync('./src/views', './dist/views');
 
 // transpile the typescript files
 childProcess.exec('tsc --build tsconfig.prod.json');
